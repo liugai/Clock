8968a86cf861ee7ca2d1061475ca5c9d8ee29e32
humor <1477650746@qq.com>
2017-07-13 15:20:58 +0800

    Initial Commit

---
+++ Clock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Clock
+//
+//  Created by 刘盖 on 2017/7/13.
+//  Copyright © 2017年 刘盖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Clock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Clock
+//
+//  Created by 刘盖 on 2017/7/13.
+//  Copyright © 2017年 刘盖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
